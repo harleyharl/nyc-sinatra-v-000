5e6774681bbb598c30726f96248ba9c8736aeb6d
Harley Rayner <harleymteden@gmail.com>
2018-12-16 21:56:52 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,5 +1,10 @@
 class LandmarksController < ApplicationController
   # add controller methods
-  get '/landmarks' do 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
   end
+
+
+
 end
