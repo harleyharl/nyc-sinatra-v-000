d779442ab01ab46d5341243a2b2e70cdf8be38bf
Harley Rayner <harleymteden@gmail.com>
2018-12-16 23:04:08 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -51,7 +51,7 @@ class LandmarksController < ApplicationController
     end
 
     if params[:landmark][:year_completed] && !params[:landmark][:year_completed].empty?
-      @landmark.name = params[:landmark][:name]
+      @landmark.year_completed = params[:landmark][:name]
     end
 
     @landmark.save
