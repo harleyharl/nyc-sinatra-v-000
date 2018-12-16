fe8b0d891013d29c4990f9e4cba891d559d70ee4
Harley Rayner <harleymteden@gmail.com>
2018-12-16 22:34:24 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -14,9 +14,9 @@ class LandmarksController < ApplicationController
     binding.pry
     @landmark = Landmark.find_by(id: params[:id])
     # @landmark.figure # @landmark.figure = [:landmark][:figure]
-    if params[:landmark][:name]
+    if !params[:landmark][:name].empty?
       @landmark.name = params[:landmark][:name]
-    end 
+    end
 
     erb :'/landmarks/edit'
   end
